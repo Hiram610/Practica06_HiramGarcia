ea46b55351ee9e108a4d0acd5344c56d6de78c41
Hiram610 <hiram.garcia216629@potros.itson.edu.mx>
2024-02-29 16:03:18 -0700

    Proyecto subido

---
+++ app/src/main/java/mx/edu/potros/practica6_216629/Pelicula.kt
@@ -0,0 +1,6 @@
+package mx.edu.potros.practica6_216629
+
+data class Pelicula(var titulo: String,
+                    var image: Int,
+                    var header: Int,
+                    var sinopsis: String)
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practica6_216629"
+include(":app")
+ 

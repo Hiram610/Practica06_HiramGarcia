8ad18d60db76ca13bbb890b41870042386a6ac74
Hiram610 <hiram.garcia216629@potros.itson.edu.mx>
2024-03-07 15:45:29 -0700

    Practica07-ChoosingMySeats

---
+++ app/src/main/java/mx/edu/potros/practica6_216629/Cliente.kt
@@ -0,0 +1,5 @@
+package mx.edu.potros.practica6_216629
+
+data class Cliente(var nombre: String, var tipoPago:String, var asiento:Int){
+
+}
--- app/src/main/java/mx/edu/potros/practica6_216629/Detalle_pelicula.kt
+++ app/src/main/java/mx/edu/potros/practica6_216629/Detalle_pelicula.kt
@@ -1,7 +1,9 @@
 package mx.edu.potros.practica6_216629
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -13,12 +15,20 @@ class Detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc : TextView = findViewById(R.id.tv_pelicula_desc)
+        val tv_seats_availale: TextView = findViewById(R.id.seatLeft)
+        val buy_ticket_button: Button = findViewById(R.id.buyTickets)
 
         val bundle = intent.extras
         if(bundle != null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            tv_seats_availale.setText(bundle.getInt("numberSeats").toString()+" "+tv_seats_availale.text)
+        }
+
+        buy_ticket_button.setOnClickListener {
+            var intent: Intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
         }
     }
 }
